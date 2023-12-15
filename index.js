4cfc73d5bfa72e4e4d2e2931a3f189bd5dffe3e6
Razdor13 <oneamaday@gmail.com>
2023-12-15 21:04:52 +0200

    done

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function sum() {
+const sum = (function () {
     let totalsum = 0;
     return function numberSum(b) {
         totalsum += b;
         return totalsum;
     };
-}
+})();
 
-const wtf = sum();
-
-console.log(wtf(3));
-console.log(wtf(5));
-console.log(wtf(20));
-console.log(wtf(20));
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
+console.log(sum(20));
